1006c2e0a7af2e8afd03ef400107a5709247110e
Alexandre <asilvaperoba@gmail.com>
2026-05-12 17:00:06 -0300

    ex00 done

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,42 +1,118 @@
 #include "ScalarConverter.hpp"
 
 
+
 ScalarConverter::ScalarConverter()
 {
 }
 
-void ScalarConverter::convert(const std::string value)
+static bool isPseudoLiteral(const std::string& value)
 {
-	double num_double = std::stod(value);
-	
-	if (std::isnan(num_double))
-		std::cout << "double: impossible" << std::endl;
+	return (value == "nan" || value == "nanf" || value == "+inf" || value == "+inff" || value == "-inf" || value == "-inff");
+}
+
+static bool isChar(const std::string& value)
+{
+	return (value.length() == 1 && !std::isdigit(value[0]));
+}
+
+static void convertChar(double num_double)
+{
+
+	if (std::isnan(num_double) || std::isinf(num_double) || num_double > 127 || num_double < 0)
+	{
+		std::cout << "char: impossible" << std::endl;
+		return ;
+	}
+
+	char c = static_cast<char>(num_double);
+
+	if (!std::isprint(c))
+		std::cout << "char: Non displayable" << std::endl;
 	else
-		std::cout << "double: " << num_double << std::endl;
+		std::cout << "char: '" << c << "'" << std::endl;
+}
 
+static void convertInt(double num_double)
+{
+	if (std::isnan(num_double) || std::isinf(num_double)
+		|| num_double > INT_MAX || num_double < INT_MIN)
+	{
+		std::cout << "int: impossible" << std::endl;
+		return ;
+	}
+	std::cout << "int: " << static_cast<int>(num_double) << std::endl;
+}
 
+static void convertFloat(double num_double)
+{
 	float num_float = static_cast<float>(num_double);
 	
-	if (std::isinf(num_float))
+	if (std::isinf(num_float) && !std::isinf(num_double))
+	{
 		std::cout << "float: impossible" << std::endl;
-	else
-		std::cout << "float: " << num_float << std::endl;
-	
+		return ;
+	}
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "float: " << num_float << "f" << std::endl;
+}
 
-	int	num_int = static_cast<int>(num_double);
+static void convertDouble(double num_double)
+{
+	if (std::isnan(num_double))
+	{
+		std::cout << "double: impossible" << std::endl;
+		return ;
+	}
 
-	if (!isalnum(num_int))
-		std::cout << "int: impossible" << std::endl;
-	else
-		std::cout << "int: " << num_int << std::endl;
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "double: " << num_double << std::endl;
+}
 
+void ScalarConverter::convert(const std::string& value)
+{
 
-	if (num_int < 0 || num_int > 127)
+	if (isPseudoLiteral(value))
+	{
 		std::cout << "char: impossible" << std::endl;
-	else if (!isprint(num_int))
-		std::cout << "char: Non displayble" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+
+		if (value[value.length() - 1] == 'f')
+		{
+			std::cout << "float: " << value << std::endl;
+			std::cout << "double: " << value.substr(0, value.length() - 1) << std::endl;
+		}
+		else
+		{
+			std::cout << "float: " << value << "f" << std::endl;
+			std::cout << "double: " << value << std::endl;
+		}
+		return ;
+	}
+
+	if (isChar(value))
+	{
+		double num_double = static_cast<double>(value[0]);
+		convertChar(num_double);
+		convertInt(num_double);
+		convertFloat(num_double);
+		convertDouble(num_double);
+	}
 	else
-		std::cout << "char:" << static_cast<char>(num_int) << std::endl;
+	{
+		char* end;
+		double num_double = std::strtod(value.c_str(), &end);
+
+		if (*end != '\0' && !(*end == 'f' && *(end + 1) == '\0'))
+		{
+    		std::cout << "Invalid literal" << std::endl;
+    		return ;
+		}
+		convertChar(num_double);
+		convertInt(num_double);
+		convertFloat(num_double);
+		convertDouble(num_double);
+	}
 }
 
 ScalarConverter::~ScalarConverter()
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -3,13 +3,23 @@
 
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <iomanip>
 
 class ScalarConverter
 {
 private:
 	ScalarConverter();
+	~ScalarConverter();
+
+	static bool isPseudoLiteral(const std::string& value);
+	static void convertChar(double num_double);
+	static void convertInt(double num_double);
+	static void convertFloat(double num_double);
+	static void convertDouble(double num_double);
 public:
-	static void convert(const std::string value);
+	static void convert(const std::string& value);
 };
 
 #endif
